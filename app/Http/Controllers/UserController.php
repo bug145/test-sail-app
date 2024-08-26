a383747021a92d5c08f7fe6115729c0c288276dd
bagdaulet <bagdaulet.front@gmail.com>
2024-08-26 18:41:49 +0500

    chore: some step

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,40 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\User;
 
-class UserController extends Controller
+class UserController extends BaseController
 {
-    //
+    public function index()
+    {
+        $user = auth()
+            ->guard('sanctum')
+            ->user()
+            ->withCount('posts')
+            ->withCount('comments')
+            ->get();
+
+        return $this->sendResponse($user, 'User info');
+    }
+
+    public function show(User $user)
+    {
+        $user = $user
+            ->withCount('posts')
+            ->withCount('comments')
+            ->get()
+            ->first()
+            ->makeHidden(['email_verified_at', 'updated_at', 'email']);
+
+        return $this->sendResponse($user, 'User info');
+    }
+
+    public function update()
+    {
+        $user = auth()
+            ->guard('sanctum')
+            ->user();
+
+        return $this->sendResponse($user, 'User info');
+    }
 }
