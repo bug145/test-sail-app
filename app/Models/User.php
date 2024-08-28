f3039c73d3698f92ba8e4b4e1838f0b045913934
bagdaulet <bagdaulet.front@gmail.com>
2024-08-28 17:25:29 +0500

    fix: user avatar

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,7 +53,7 @@ class User extends Authenticatable
     protected function avatar(): Attribute
     {
         return Attribute::make(
-            get: fn(string $value) => Storage::disk('minio')->url($value),
+            get: fn(string $value) => $value ? Storage::disk('minio')->url($value) : '',
         );
     }
 
